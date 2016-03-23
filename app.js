613008e02a7e33506087135f1608332860caebaf
Dmitri Zagidulin <dzagidulin@gmail.com>
2016-03-23 13:51:49 +0300

    Add loadProfile() call

--- app.js
+++ app.js
@@ -14,8 +14,15 @@ function login () {
   if (webId) {
     solid.login(webId).then(function (loggedInWebId) {
       console.log('Welcome user: %s', loggedInWebId)
+      return loggedInWebId
     }).catch(function (reason) {
       console.log('Unable to make a HEAD request for login: %o', reason)
+    })
+    .then(function (webId) {
+      return solid.getProfile(webId)
+    })
+    .then(function (profile) {
+      console.log('Here is your profile: %o', profile)
     })
   }
 }
